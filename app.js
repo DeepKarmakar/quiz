dfa97c0557045e85ba115890f199d82284ea2d81
Deep Karmakar <deepkarmakar61@gmail.com>
2016-11-30 18:39:27 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,4 +3,26 @@
 *
 * Description
 */
-angular.module('quiz', ['ngRoute']);
+var myApp =angular.module('quiz', ['ngRoute']);
+					
+    myApp.config(function($routeProvider, $locationProvider) {
+	    $routeProvider
+	        .when('/home', {
+	            templateUrl: 'view/home.html',
+	            controller: 'homeController'
+	        })
+	        .when('/game', {
+	            templateUrl: 'view/quiz.html',
+	            controller: 'quizController'
+	        }) 
+	        .when('/game/:id', {
+	        	templateUrl: 'view/quiz-single.html',
+	        	controller: 'sigleQuizController'
+	        })	        
+	        .otherwise({
+	            redirectTo: '/home'
+	        });
+
+	    $locationProvider.html5Mode(true)
+
+    });
